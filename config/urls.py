fa3e74586dc6763bc0b469faf7d4cf4f41758ec5
Khaled Kamel <Khaled Kamel@DESKTOP-CLI1CDM>
2025-12-08 23:11:15 +0200

    Modified urls.py: fixed home page error

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from api.views import IDVerificationView
+from api.views import IDVerificationView, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
